dc29bffd44239221d31b4f9e929a678742a1e89f
jparev <johnpaul178@revature.net>
2025-01-13 13:06:31 -0600

    updated test suite, generated report

--- src/test/java/com/revature/PlanetariumTestSuite.java
+++ src/test/java/com/revature/PlanetariumTestSuite.java
@@ -2,6 +2,7 @@ package com.revature;
 
 import com.revature.tests.repository.moon.MoonRepositoryDeletionTest;
 import com.revature.tests.repository.planet.PlanetRepositoryDeletionTest;
+import com.revature.tests.repository.planet.PlanetRetrievalRepositoryTest;
 import com.revature.tests.service.moon.MoonServiceDeletionTest;
 import com.revature.tests.service.planet.PlanetServiceDeletionTest;
 import com.revature.tests.repository.moon.MoonCreationDaoTest;
@@ -13,31 +14,41 @@ import com.revature.tests.service.moon.MoonCreationServiceTest;
 import com.revature.tests.service.moon.MoonRetrievalServiceTest;
 import com.revature.tests.service.planet.PlanetCreationServiceTest;
 import com.revature.tests.service.planet.PlanetRetrievalServiceTest;
+import com.revature.tests.service.user.UserServiceAuthentication;
 import com.revature.tests.service.user.UserServiceCreationTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-        MoonRepositoryDeletionTest.class,
-        PlanetRepositoryDeletionTest.class,
-        MoonServiceDeletionTest.class,
-        PlanetServiceDeletionTest.class,
-
-        MoonRetrievalRepositoryTest.class,
-        PlanetRetrievalServiceTest.class,
-        MoonRetrievalServiceTest.class,
-        PlanetRetrievalServiceTest.class,
-
+        //repository/moon
         MoonCreationDaoTest.class,
-        PlanetCreationDaoTest.class,
-        MoonCreationServiceTest.class,
-        PlanetCreationServiceTest.class,
+        MoonRepositoryDeletionTest.class,
+        MoonRetrievalRepositoryTest.class,
 
+        //repository/planet
+        PlanetCreationDaoTest.class,
+        PlanetRepositoryDeletionTest.class,
+        PlanetRetrievalRepositoryTest.class,
+
+        //repository/user
         UserDaoCreationTest.class,
         UserDaoRetrievalTest.class,
-        UserServiceCreationTest.class,
-        UserDaoRetrievalTest.class
+
+        //service/moon
+        MoonCreationServiceTest.class,
+        MoonRetrievalServiceTest.class,
+        MoonServiceDeletionTest.class,
+
+        //service/planet
+        PlanetCreationServiceTest.class,
+        PlanetRetrievalServiceTest.class,
+        PlanetServiceDeletionTest.class,
+
+        //service/user
+        UserServiceAuthentication.class,
+        UserServiceCreationTest.class
+
 })
 public class PlanetariumTestSuite {
 
