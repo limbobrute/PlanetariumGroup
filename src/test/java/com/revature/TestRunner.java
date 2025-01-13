69448df51f7eb97b57700c638486918c23191544
LimboBrute <wihprahl@gmail.com>
2025-01-13 11:12:35 -0600

    Undo update

--- src/test/java/com/revature/TestRunner.java
+++ src/test/java/com/revature/TestRunner.java
@@ -8,29 +8,17 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
 
-@RunWith(Suite.class)
-@Suite.SuiteClasses(
-    {
-        MoonCreationDaoTest.class,
-        MoonCreationServiceTest.class,
-        PlanetCreationDaoTest.class,
-        PlanetCreationServiceTest.class
-    }
-)
 
-/*
 @CucumberOptions(
         features = "classpath:features",
         glue = "com.revature.tests",
         plugin = {"pretty", "html:src/test/resources/reports/html-report.html", "json:src/test/resources/reports/json-report.json"}
 )
-*/
+
 public class TestRunner
 {
-    /*
+
     @BeforeClass
     public static void setup()
     {
@@ -42,5 +30,5 @@ public class TestRunner
     {
         //Reset everything here as needed
     }
-    */
+
 }
