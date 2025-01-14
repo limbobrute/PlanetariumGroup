5c274f6a19c33134ae84230491a01c61caeb5731
LimboBrute <wihprahl@gmail.com>
2025-01-14 11:32:57 -0600

    Fixed Moon Creation tests
    Was missing mocking certain methods in the mock object.

--- src/test/java/com/revature/tests/service/moon/MoonCreationServiceTest.java
+++ src/test/java/com/revature/tests/service/moon/MoonCreationServiceTest.java
@@ -32,7 +32,7 @@ public class MoonCreationServiceTest
     Moon NonUniqueName;
     Moon BadImage;
 
-    Moon MockMoon;
+    //Moon MockMoon;
 
     @Rule
     public ExpectedException thrown = ExpectedException.none();
@@ -54,6 +54,7 @@ public class MoonCreationServiceTest
         InvalidPlanetId = new Moon(0, "BadId", 0);
         NonUniqueName = new Moon(0, "Titan", 1);
         BadImage = new Moon(0, "BadImage", 1);
+        //MockMoon = new Moon(3, "", 1);
         ServiceTestMoon.setImageData(jpgFile);
         BadImage.setImageData(webpFile);
 
@@ -64,6 +65,7 @@ public class MoonCreationServiceTest
     public void ServiceLayerMoonCreationSuccess()
     {
         //Looking for boolean primitive set to TRUE
+        Moon MockMoon = new Moon(3, "Test", 1);
         Mockito.when(DaoObject.createMoon(ServiceTestMoon)).thenReturn(Optional.ofNullable(MockMoon));
         Moon NewMoon = MoonService.createMoon(ServiceTestMoon);
         Assert.assertEquals(3,NewMoon.getMoonId());
@@ -72,15 +74,18 @@ public class MoonCreationServiceTest
     @Test
     public void ServiceLayerNonUniqueName()
     {
+        Moon MockMoon = new Moon(0, "Titan", 1);
         thrown.expect(MoonFail.class);
         thrown.expectMessage("unique name fail");
         Mockito.when(DaoObject.createMoon(NonUniqueName)).thenReturn(Optional.ofNullable(MockMoon));
+        Mockito.when(DaoObject.readMoon(MockMoon.getMoonName())).thenReturn(Optional.ofNullable(MockMoon));
         Moon NewMoon = MoonService.createMoon(NonUniqueName);
     }
 
     @Test
     public void ServiceLayerInvalidMoonName()
     {
+        Moon MockMoon = new Moon (0, "Test!!!", 1);
         thrown.expect(MoonFail.class);
         thrown.expectMessage("Invalid moon name");
         Mockito.when(DaoObject.createMoon(InvalidName)).thenReturn(Optional.ofNullable(MockMoon));
@@ -91,6 +96,7 @@ public class MoonCreationServiceTest
     public void ServiceLayerTooManyCharacters()
     {
         //Looking for unhandled MoonFail Exception with message "character length fail"
+        Moon MockMoon = new Moon(0, "thisissomanycharacterswhyisthisresitrctioninplace", 1);
         thrown.expect(MoonFail.class);
         thrown.expectMessage("character length fail");
         Mockito.when(DaoObject.createMoon(TooManyCharacters)).thenReturn(Optional.ofNullable(MockMoon));
@@ -103,6 +109,8 @@ public class MoonCreationServiceTest
     public void ServiceLayerInvalidMoonImage()
     {
         //Looking for unhandled MoonFail Exception with message "Invalid file type"
+        Moon MockMoon = new Moon(0, "BadImage", 1);
+        MockMoon.setImageData(webpFile);
         thrown.expect(MoonFail.class);
         thrown.expectMessage("Invalid file type");
         Mockito.when(DaoObject.createMoon(BadImage)).thenReturn(Optional.ofNullable(MockMoon));
