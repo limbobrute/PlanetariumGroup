93ef8673533969ee7c927882104117b57c339f91
jparev <johnpaul178@revature.net>
2025-01-22 12:23:26 -0600

    updated setup-reset for postgres

--- src/test/resources/setup-reset.sql
+++ src/test/resources/setup-reset.sql
@@ -1,7 +1,7 @@
 -- Use this script to set up your Planetarium database: do not edit the script
 
 -- needed for referential integrity enforcement if executing the queries manually
-PRAGMA foreign_keys = ON;
+--PRAGMA foreign_keys = ON;
 
 drop table if exists moons;
 
@@ -10,42 +10,42 @@ drop table if exists planets;
 drop table if exists users;
 
 create table users(
-	id integer primary key,
+	id integer primary key GENERATED ALWAYS AS IDENTITY,
 	username text unique not null,
 	password text not null,
 	constraint username_length_check check (
-		length(username) >= 5 and 
+		length(username) >= 5 and
 		length(username) <= 30
 	),
 	constraint password_length_check check (
-		length(password) >= 5 and 
+		length(password) >= 5 and
 		length(password) <= 30
 	),
 	constraint username_character_check check (
-		username GLOB '[a-zA-Z]*' AND 
-		username not glob '*[^a-zA-Z0-9_-]*'
+		username SIMILAR TO '[a-zA-Z]%' AND
+		username not SIMILAR TO '%[^a-zA-Z0-9_-]%'
 	),
 	constraint password_character_check check (
-		password GLOB '*[a-z]*' and
-		password GLOB '*[A-Z]*' and 
-		password GLOB '*[0-9]*' and
-		password GLOB '[a-zA-Z]*' AND 
-		username not glob '*[^a-zA-Z0-9_-]*'
+		password SIMILAR TO '%[a-z]%' and
+		password SIMILAR TO '%[A-Z]%' and
+		password SIMILAR TO '%[0-9]%' and
+		password SIMILAR TO '[a-zA-Z]%' AND
+		username not SIMILAR TO '%[^a-zA-Z0-9_-]%'
 	)
 );
 
 insert into users (username, password) values ('Batman', 'Iamthenight1939');
 
 create table planets(
-	id integer primary key,
+	id integer primary key GENERATED ALWAYS AS IDENTITY,
 	name text unique not null,
 	ownerId integer not null,
-	image blob,
+	image bytea,
 	foreign key(ownerId) references users(id) on delete restrict,
 	constraint name_length_check check (length(name) <= 30),
 	constraint name_character_check check (
-		name GLOB '[a-zA-Z]*' and
-		name not GLOB '*[^a-zA-Z0-9_ -]*'
+		name SIMILAR TO '[a-zA-Z]%' and
+		name not SIMILAR TO '%[^a-zA-Z0-9_ -]%'
 	)
 );
 
@@ -53,15 +53,15 @@ insert into planets (name, ownerId, image) values ('Earth', 1, ?);
 insert into planets (name, ownerId, image) values ('Mars', 1, ?);
 
 create table moons(
-	id integer primary key,
+	id integer primary key GENERATED ALWAYS AS IDENTITY,
 	name text unique not null,
 	myPlanetId integer not null,
-	image blob,
+	image bytea,
 	foreign key(myPlanetId) references planets(id) on delete cascade,
 	constraint name_length_check check (length(name) <= 30),
 	constraint name_character_check check (
-		name GLOB '[a-zA-Z]*' AND
-		name not GLOB '*[^a-zA-Z0-9_ -]*'
+		name SIMILAR TO '[a-zA-Z]%' AND
+		name not SIMILAR TO '%[^a-zA-Z0-9_ -]%'
 	)
 );
 
