0985d83670a1d1bd10310b2613405b466f145fb3
jparev <johnpaul178@revature.net>
2025-01-29 15:10:37 -0600

    e2e test

--- src/test/java/com/revature/TestRunner.java
+++ src/test/java/com/revature/TestRunner.java
@@ -38,9 +38,9 @@ public class TestRunner {
     public static void setup(){
         ChromeOptions options = new ChromeOptions();
         options.addArguments("headless");
-        String tempDir = "chrome-profile-" + System.getenv("BUILD_ID");
-        options.addArguments("user-data-dir=" + tempDir);
-        driver = new ChromeDriver();
+        //String tempDir = "chrome-profile-" + System.getenv("BUILD_ID");
+        //options.addArguments("user-data-dir=" + tempDir);
+        driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
         wait = new WebDriverWait(driver, Duration.ofSeconds(1));
         homePage = new HomePage(driver);
