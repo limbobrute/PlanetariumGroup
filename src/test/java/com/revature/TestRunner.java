ed67b3f9832a8d5daa4b94f08b4a4e3aac5532a0
jparev <johnpaul178@revature.net>
2025-01-29 15:32:47 -0600

    e2e test

--- src/test/java/com/revature/TestRunner.java
+++ src/test/java/com/revature/TestRunner.java
@@ -40,7 +40,7 @@ public class TestRunner {
         options.addArguments("--headless");
         options.addArguments("--disable-gpu");
         options.addArguments("--no-sandbox");
-        //options.addArguments("--user-data-dir=/tmp/user-data" + System.getenv("BUILD_ID"));
+        options.addArguments("--user-data-dir=/tmp/chrome/user-data" + System.getenv("BUILD_ID"));
         driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
         wait = new WebDriverWait(driver, Duration.ofSeconds(1));
