30df857cefb250d16db6238636456c8ed1066fa5
mickestiler <mickestiler@gmail.com>
2025-01-29 21:19:29 -0500

    pipeline test in databaseconnector

--- src/test/java/com/revature/utils/DatabaseConnector.java
+++ src/test/java/com/revature/utils/DatabaseConnector.java
@@ -6,22 +6,14 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.DriverManager;
 
-public class DatabaseConnector {
-    private static final String DB_URL  = System.getenv("DATABASE_URL");
-    private static final String DB_USER = System.getenv("DB_USER");
-    private static final String DB_PASS = System.getenv("DB_PASS");
-
-    public static Connection getConnection() throws SQLException {
-        // Print the URL & user to confirm what you’re using:
-        System.out.println("Attempting DB connection to: " + DB_URL);
-        System.out.println("Using user: " + DB_USER);
-
-        // Actually connect
-        Connection conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASS);
-
-        // If successful:
-        System.out.println("DB connection established successfully!");
-        return conn;
+public class DatabaseConnector
+{
+    public static Connection getConnection() throws SQLException
+    {
+        String url = System.getenv("RDS_URL");
+        String username = System.getenv("RDS_USERNAME");
+        String password = System.getenv("RDS_PASSWORD");
+        System.out.println("rds_username: " + username);
+        return DriverManager.getConnection(url, username, password);
     }
 }
-
