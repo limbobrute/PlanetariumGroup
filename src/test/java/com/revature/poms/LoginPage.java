cdbb95d64facfe6dd887a4fb553a9567f094ce00
mickestiler <mickestiler@gmail.com>
2025-01-29 19:25:31 -0500

    pipeline test in setUpLoggedInUser

--- src/test/java/com/revature/poms/LoginPage.java
+++ src/test/java/com/revature/poms/LoginPage.java
@@ -28,7 +28,7 @@ public class LoginPage {
 
     public void setUpLoggedInUser(){
         driver.get("http://localhost:8081/");
-        driver.getPageSource();
+        System.out.println(driver.getPageSource());
         usernameInput.sendKeys("Batman");
         passwordInput.sendKeys("Iamthenight1939");
         loginButton.submit();
